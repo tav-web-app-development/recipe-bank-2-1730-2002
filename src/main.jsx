b57769d242f222b2a83c116475a190796896816c
ishita <ishita.email@example.com>
2024-05-07 23:55:31 -0400

    Done

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NewRecipe from "./Components/NewRecipe.jsx";
+import Layout from "./Components/Layout.jsx";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/newRecipe",
+        element: <NewRecipe />,
+      },
+    ],
+  },
+]);
